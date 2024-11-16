ad2555b237083ab6da2d4f4c089499f99e625759
vansh-suneja <vanshsuneja190@gmail.com>
2024-11-16 17:30:35 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectionDB = require("./config/db");
+const userRoutes = require("./routes/user.routes")
+const chatRoutes = require("./routes/chat.routes")
+const messageRoutes = require("./routes/message.routes")
+
+const cors = require('cors');
+
+const app = express();
+dotenv.config();
+connectionDB();
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log("welcome to chat app");
+})
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+const port = process.env.PORT || 8001
+const server = app.listen(port, console.log("server is running at post = ", port));
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "https://chat-app-ca.netlify.app", 
+    },
+});
+
+io.on("connection", (socket) => {
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit("connected")
+    })
+
+    socket.on("join chat", (room) => {
+        socket.join(room)
+
+    })
+
+    socket.on("new message", (newMessageRec) => {
+        var chat = newMessageRec.chat;
+        if (!chat.users) return console.log("chat user not defined");
+
+        chat.users.forEach(user => {
+            if (user != newMessageRec.sender._id) {
+                socket.in(user).emit("message received", newMessageRec);
+            }
+        });
+    });
+
+
+
+});
---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import AllChats from "../components/Home/AllChats";
+import ChatBox from "../components/Home/ChatBox";
+import { io } from "socket.io-client";
+import * as types from "../redux/appReducer/actionType";
+import { useDispatch } from "react-redux";
+
+const ENDPOINT="https://chatc.onrender.com"
+
+const Home = () => {
+  const [socketConnected, setSocketConnected] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const socket = io(ENDPOINT);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const userData = JSON.parse(
+      localStorage.getItem("chat-app-login-user-data")
+    );
+
+    socket.on("connect", () => {
+      setIsLoading(false);
+    });
+
+    socket.emit("setup", userData);
+    socket.on("connection", () => setSocketConnected(true));
+
+    dispatch({ type: types.WEB_SOCKET_CONNECTED, payload: socket });
+
+  }, []);
+
+  return (
+    <div className="flex flex-wrap justify-between h-screen max-h-full">
+      <div className="w-full h-full lg:w-1/4 bg-primary-400 hidden lg:block">
+        {/* Conditional rendering based on the socket connection status */}
+        {isLoading ? (
+          <p></p>
+        ) : (
+          <AllChats />
+        )}
+      </div>
+      <div className="w-full h-full lg:w-3/4 bg-primary-400 p-4">
+        {/* Conditional rendering based on the socket connection status */}
+        {isLoading ? (
+          <p></p>
+        ) : (
+          <ChatBox />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
